4765d26978fa22907d75f92dd4bfe2a1c0918fd3
sergey alaverdyan <sergalaverdyan@gmail.com>
2022-11-30 11:37:36 +0400

    my task

---
+++ src/components/MyCarousel/index.js
@@ -0,0 +1,48 @@
+import { Carousel } from 'antd';
+import { useRef } from "react";
+
+import './styles.css';
+
+const MyCarousel = () => {
+    const carouselRef = useRef(null);
+
+
+    const arrowPress = (event) => {
+        if (event === 'prev') {
+            carouselRef.current.prev();
+        } else {
+            carouselRef.current.next();
+        }
+    }
+
+
+
+    return (
+        <div>
+            <div className='arrows-container'>
+                <p onClick={() => {
+                    arrowPress('prev')
+                }}>Prev</p>
+                <p onClick={() => {
+                    arrowPress('next')
+                }}>Next</p>
+            </div>
+            <Carousel ref={carouselRef}>
+                <div>
+                    <h3 className='content-style'>1</h3>
+                </div>
+                <div>
+                    <h3 className='content-style'>2</h3>
+                </div>
+                <div>
+                    <h3 className='content-style'>3</h3>
+                </div>
+                <div>
+                    <h3 className='content-style'>4</h3>
+                </div>
+            </Carousel>
+        </div>
+    )
+}
+
+export default MyCarousel;
